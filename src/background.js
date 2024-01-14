f11fe4ed6486a6b7f8d74516d50947625a7876c9
paruba <j.petr.paroubek@gmail.com>
2024-01-14 21:16:26 +0100

    added popup window

--- src/background.js
+++ src/background.js
@@ -1,15 +1,20 @@
 import { Document, Packer, Paragraph, ImageRun, TextRun } from 'docx';
 
-let storedScreenshots = []; // Array to store screenshots
+let storedScreenshots = [];
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.commands.onCommand.addListener(function(command) {
-        if (command === "take_screenshot") {
-            takeScreenshot();
-        } else if (command === "manage_screenshots") {
-            manageScreenshots();
-        }
-    });
+const popupHtml = `
+<div id="screenshotPopup" style="position: fixed; top: 10px; right: 10px; z-index: 1000; background-color: #f8f8f8; border: 1px solid #ddd; padding: 10px; box-shadow: 0px 0px 5px #ccc;">
+    Screenshot taken and stored!
+</div>
+`;
+
+
+chrome.commands.onCommand.addListener(function(command) {
+    if (command === "take_screenshot") {
+        takeScreenshot();
+    } else if (command === "manage_screenshots") {
+        manageScreenshots();
+    }
 });
 
 function takeScreenshot() {
@@ -19,8 +24,34 @@ function takeScreenshot() {
             return;
         }
         console.log("Screenshot taken:", imageUri);
-        storedScreenshots.push(imageUri); // Store screenshot in memory
+        
+        storedScreenshots.push(imageUri);
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            const currentTabId = tabs[0].id;
+
+            // Inject the popup HTML into the current tab
+            chrome.scripting.executeScript({
+                target: { tabId: currentTabId },
+                function: showPopup
+            });
+        });
     });
+}
+
+function showPopup() {
+    const popupHtml = `
+    <div id="screenshotPopup" style="position: fixed; top: 10px; right: 10px; z-index: 1000; background-color: #f8f8f8; border: 1px solid #ddd; padding: 10px; box-shadow: 0px 0px 5px #ccc;">
+        Screenshot taken and stored!
+    </div>
+    `;
+
+    const div = document.createElement('div');
+    div.innerHTML = popupHtml;
+    document.body.appendChild(div);
+
+    setTimeout(() => {
+        div.remove();
+    }, 1000);
 }
 
 function manageScreenshots() {
